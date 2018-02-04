ef89c515ec84755d9ca1e876a95c452e8ea4e199
Rodrigo Cervantes <rodrigocervantez@gmail.com>
2018-02-03 23:18:51 -0600

    Optimization

--- app/Sites/blog/modules/Posts/helper/Posts.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace rdks\app\sites\blog\modules\Posts\helper;
-
-use rdks\app\sites\admin\modules\Posts\helper\Posts as PostsAdminHelper;
-
-class Posts extends PostsAdminHelper {
-
-}
