f36b613827309848661976440534bd7fcf68790d
Rodrigo Cervantes <rodrigocervantez@gmail.com>
2018-02-06 10:13:52 -0600

    Update interfaces

--- core/Interfaces/JSONInterface.php
+++ core/Interfaces/JSONInterface.php
@@ -20,31 +20,7 @@
 
 namespace Roducks\Interfaces;
 
-Interface ApiInterface
+Interface JSONInterface
 {
-	/**
-	 * @type GET
-	 */
-	public function row($id);
-
-	/**
-	 * @type GET
-	 */
-	public function getAll(\stdClass $request);
-
-	/**
-	 * @type POST
-	 */
-	public function store(\stdClass $request);
-
-	/**
-	 * @type PUT
-	 */
-	public function update(\stdClass $request, $id);
-
-	/**
-	 * @type DELETE
-	 */
-	public function remove(\stdClass $request, $id);
-
+	public function __construct(array $settings);
 }
