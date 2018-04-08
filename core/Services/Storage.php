57289575c67a4e179e99963a913c685d222eae60
Rodrigo Cervantes <rodrigocervantez@gmail.com>
2018-04-08 18:33:48 -0500

    Improvement for Storage service

--- core/Services/Storage.php
+++ core/Services/Storage.php
@@ -20,14 +20,224 @@
 
 namespace Roducks\Services;
 
-class Storage extends Uploader
+use Roducks\Page\Service;
+use Roducks\Page\JSON;
+use Roducks\Framework\Dispatch;
+use Roducks\Framework\Helper;
+use Roducks\Framework\Path;
+use Roducks\Libs\Files\Image;
+use Roducks\Libs\Files\File;
+use Roducks\Libs\Files\Directory;
+use Roducks\Libs\Utils\Date;
+
+class Storage extends Service
 {
+
+	protected $_dispatchUrl = true;
+
+	private $_ajax = true,
+			$_response = [],
+			$_input = "uploader";
+
+	private function _serviceError($code, $msg)
+	{
+		if ($this->_ajax) {
+			$this->setError($code, $msg);
+		}
+	}
+
+	private function __makeCrops($fx, $dir, $f, array $cuts = [])
+	{
+
+		$w = $this->post->param("w");
+		$h = $this->post->param("h");
+		$x = $this->post->param("x");
+		$y = $this->post->param("y");
+
+		/* --- CROP ---- */
+		$fn = explode(".", $f);
+		$full_image = $fn[0] . '_'. $fx .'.' . $fn[1];
+		$image = new Image;
+		$image->crop($dir . $f, $dir . $full_image, $w, $h, $x, $y, 95);
+
+		if(count($cuts) > 0):
+			foreach($cuts as $c):
+				$preset = $fn[0] . '_' . $c . '.' . $fn[1];
+				if($w >= $c): // if crop is greather than resize
+					$image->load($dir . $full_image);
+					$image->resizeToWidth($c);
+					$image->save($dir . $preset, 95);	
+				endif;
+			endforeach;	
+		endif;
+	
+	}
+
+	// delete old image when uploading a new one.
+	private function __deleteCrops($path, array $cuts = [])
+	{
+		$file = File::init();
+
+		$copy = $this->post->param('copy');
+
+		if(!empty($copy)):
+			$file->delete($path, $copy);
+			// delte all the crops
+			if(count($cuts) > 0): foreach($cuts as $c):
+				$file->delete($path, Path::getCropName($copy, $c));
+			endforeach; endif;
+		endif;			
+	}
+
+	private function _deleteCrops($path, array $cuts = [])
+	{
+		$this->__deleteCrops($path, $cuts);
+		parent::output();
+	}	
+	
+	private function _crop($fx, $dir, $dir2, $cuts)
+	{
+		
+		$file = $this->post->param("cropper");
+		$this->__makeCrops($fx, $dir, $file, $cuts);
+		$cuts = array_merge(array($fx), $cuts);
+		$this->__deleteCrops($dir, $cuts);
+
+		$data = [
+			'dir' => $dir2,
+			'img_full' => $file,
+			'img_cropped' => Path::getCropName($file, 150),
+			'name' => $file
+		];
+
+		$this->setMessage(TEXT_CHANGES_SAVED);
+		$this->data($data);
+
+		parent::output();
+	}
+	
+	private function _upload($prefix, $dir, $dir2, array $size = [], array $types = [])
+	{
+
+		$file = File::init();
+		if(count($types) > 0) $file->type($types);
+		if(count($size) > 0){
+			
+			if(strtoupper($size[1]) == 'KB'){
+				$file->kb($size[0]);	
+			}
+			
+			if(strtoupper($size[1]) == 'MB'){
+				$file->mb($size[0]);	
+			}		
+		
+		}
+		
+		// If upload directory does not exist, let's create it!
+		Directory::make($dir);
+
+		$filename = $prefix . Date::getFlatDate(Date::getCurrentDateTime());
+		$resp = $file->upload($dir, $this->_input, $filename);
+
+		$data = [
+				'success' => $resp['success'],
+				'message' => $resp['message'],
+				'code' => $resp['code'],
+				'data' => [
+					'dir' => $dir2,
+					'file' => $resp['file']
+				]	
+			];
+			
+		if ($this->_ajax) {
+			$this->data($data);
+			
+			// We don't send Http headers because of this is a post request inside of an iframe and it's parsed properly as text plain. 
+			echo JSON::encode($this->getJsonData());
+		} else {
+			$this->_response = $data;
+		}
+	
+	}
+	
+	private function _deleteFile($dir)
+	{
+		$file = File::init();
+		$file = ($this->_ajax) ? $this->post->param("file") : $this->_input;
+		$response = $file->delete($dir, $file);		
+
+		if ($this->_ajax) { 
+			parent::output();
+		} else {
+			$this->_response = $response;
+		}
+	}
+
+	public function file($action = "", $class = "", $index = "")
+	{
+
+		$this->params([
+			'action' => [$action, 'PARAM', Dispatch::PARAM_STRING],
+			'class' => [$class, 'PARAM', Dispatch::PARAM_STRING],
+			'index' => [$index, 'PARAM', Dispatch::PARAM_STRING]
+		]);
+
+		$method = Helper::getCamelName($action, false);
+		$config = $this->config($class, $index, null);
+
+		if(is_null($config)){
+			$method = "invalid";
+		}
+
+		switch ($method) {
+			case 'cropSquared':
+				if(isset($config['clipping']) && isset($config['dir_upload']) && isset($config['dir_uploaded']) && isset($config['squared_clippings'])){
+					$this->_crop($config['clipping'], \App::getRealFilePath($config['dir_upload']), $config['dir_uploaded'], $config['squared_clippings']);
+				} else {
+					$this->_serviceError(0, TEXT_SERVICE_UNAVAILABLE);
+				}
+				break;
+			case 'cropLandscape':
+				if(isset($config['clipping']) && isset($config['dir_upload']) && isset($config['dir_uploaded']) && isset($config['landscape_clippings'])){
+					$this->_crop($config['clipping'], \App::getRealFilePath($config['dir_upload']), $config['dir_uploaded'], $config['landscape_clippings']);
+				} else {
+					$this->_serviceError(0, TEXT_SERVICE_UNAVAILABLE);
+				}
+				break;
+			case 'upload':
+				if(isset($config['prefix']) && isset($config['dir_upload']) && isset($config['dir_uploaded']) && isset($config['size']) && isset($config['types'])){
+					$this->_upload($config['prefix'], \App::getRealFilePath($config['dir_upload']), $config['dir_uploaded'], $config['size'], $config['types']);
+				} else {
+					$this->_serviceError(0, TEXT_SERVICE_UNAVAILABLE);
+				}
+				break;
+			case 'delete':
+				if(isset($config['dir_upload'])){
+					$this->_deleteFile($config['dir_upload']);
+				} else {
+					$this->_serviceError(0, TEXT_SERVICE_UNAVAILABLE);
+				}
+				break;
+			case 'deleteCrops':
+				if(isset($config['dir_upload']) && isset($config['squared_clippings'])){
+					$this->_deleteCrops(\App::getRealFilePath($config['dir_upload']), $config['squared_clippings']);
+				} else {
+					$this->_serviceError(0, TEXT_SERVICE_UNAVAILABLE);
+				}
+				break;
+			default:
+				$this->setServiceError(0, TEXT_SERVICE_UNAVAILABLE);
+				if ($this->_ajax) parent::output();
+				break;												
+		}
+
+	}
 
 	public function uploadFile($input, $class, $index)
 	{
 		$this->_ajax = false;
 		$this->_input = $input;
-		$this->file($class, $index, 'upload-file');
+		$this->file('upload', $class, $index);
 
 		return $this->_response;
 	}
@@ -36,7 +246,7 @@ class Storage extends Uploader
 	{
 		$this->_ajax = false;
 		$this->_input = $input;
-		$this->file($class, $index, 'delete-file');
+		$this->file('delete', $class, $index);
 
 		return $this->_response;
 	}
